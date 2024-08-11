6ca99c60aed6603c9f1202bc9f8e02b24eee00c3
giornot12 <giornot12@nengutech.com>
2024-08-11 11:09:38 -0500

    modified git actions workflows/terraform.yml

--- variables.tf
+++ variables.tf
@@ -1,18 +1,21 @@
 variable "project_id" {
   description = "The project ID to host the cluster in"
+  type        = string
 }
 
 variable "region" {
   description = "The region to host the cluster in"
+  type        = string
   default     = "us-central1"
 }
 
 variable "zone" {
   description = "The zone to host the cluster in"
+  type        = string
   default     = "us-central1-a"
 }
 
-variable "credentials_file" {
-  description = "The path to the GCP credentials JSON file"
-  default     = "~/.config/gcloud/terraform-admin.json"
-}
+#variable "credentials_file" {
+#  description = "The path to the GCP credentials JSON file"
+#  default     = "~/.config/gcloud/terraform-admin.json"
+#}
